95fcfcf336a99f733211186a5101777477f9b210
khenryjoe <joseph.ketterer@gmail.com>
2020-04-12 18:49:09 +0100

    new

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.on('connection', socket => {
     socket.emit('message', 'Welcome to chatcord!')
 });
 
+
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
