03c5cbe9d6a104ca0672e6e769691a35406409af
khenryjoe <joseph.ketterer@gmail.com>
2020-04-12 19:44:07 +0100

    f

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ io.on('connection', socket => {
 
 const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
+server.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
