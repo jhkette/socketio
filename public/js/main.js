25ca12331e26f46e16a46712d2cdfad629a00f3e
khenryjoe <joseph.ketterer@gmail.com>
2020-04-13 13:43:33 +0100

    js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,54 @@
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+console.log(username, room)
+
 const socket = io();
+
+
+socket.emit('joinroom', {username, room})
+
+//message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+chatForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+ 
+    //emit message to server
+    socket.emit('chatMessage',msg);
+
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.value.focus();
+
+
+
+})
+
+
+//output message to DOm
+
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta"> ${message.username}<span>${message.time}</span></p>
+    <p class="text">
+       ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
+const formatMessage = require('./utlis/messages');
 
 
 const app = express();
@@ -15,18 +15,35 @@ const io = socketio(server);
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botname = 'chatbot'
+
 io.on('connection', socket => {
-    // message to the user 
-    socket.emit('message', 'Welcome to chatcord!')
+
+    socket.on('joinRoom', ({username, room})=> {
+
+            // message to the user 
+    socket.emit('message', formatMessage(botname, 'Welcome to chatcord!'))
 
 
     //broadcast whn a usr connects - but not to user
     socket.broadcast.emit('message', 'A user has joined the chat');
 
+    })
+
+
+      //listen for chatmessage
+
+      socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage(botname,msg));
+    })
+
+
     // when client disconnects
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left the chat')
     })
+
+  
 });
 
 
